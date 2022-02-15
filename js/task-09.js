f4d721b58aa9585bbb1f7ae1fb441af84030f6e8
oleksandr-milashechkin <nonstopjackfrost@gmail.com>
2022-02-16 01:36:15 +0200

    Финалочка

--- js/task-09.js
+++ js/task-09.js
@@ -1,8 +1,10 @@
+// Функция получения случайного цвета
 function getRandomHexColor() {
   console.log(`#${Math.floor(Math.random() * 16777215).toString(16)}`);
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+//Получаем ссылки на элементы разметки
 const bodyElement = document.querySelector('body');
 console.log(bodyElement);
 const changeColorBtn = document.querySelector('.change-color');
@@ -10,7 +12,7 @@ console.log(changeColorBtn);
 const currentColor = document.querySelector('.color');
 console.log(currentColor);
 
-
+// Добавляем слушателя на кнопку и прописываем обновление текста на значение нового цвета по клику
 changeColorBtn.addEventListener('click', () => {
   const changeColor = getRandomHexColor();
   bodyElement.style.backgroundColor = changeColor;
