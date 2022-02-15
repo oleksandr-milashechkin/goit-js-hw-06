39e5e55c3819301527678ec416460d18c3b60e5d
oleksandr-milashechkin <nonstopjackfrost@gmail.com>
2022-02-16 01:33:35 +0200

    Финалка
    Сделаны 9 задач

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
+  console.log(`#${Math.floor(Math.random() * 16777215).toString(16)}`);
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyElement = document.querySelector('body');
+console.log(bodyElement);
+const changeColorBtn = document.querySelector('.change-color');
+console.log(changeColorBtn);
+const currentColor = document.querySelector('.color');
+console.log(currentColor);
+
+
+changeColorBtn.addEventListener('click', () => {
+  const changeColor = getRandomHexColor();
+  bodyElement.style.backgroundColor = changeColor;
+  currentColor.textContent = bodyElement.style.backgroundColor;
+});
+
+
